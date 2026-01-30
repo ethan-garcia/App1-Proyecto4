eb3e4496f277d8de6f987fd9adde511dbb307831
ethan-garcia <ethan.garcia@iest.edu.mx>
2026-01-30 08:21:57 -0600

    Add files via upload

---
+++ Proyecto 4/Proyecto 4/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+        Dim resultado As Integer = Numero1 + Numero2
+        Label4.Text = resultado.ToString()
+    End Sub
+End Class
