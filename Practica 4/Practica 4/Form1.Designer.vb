c63be97f49e1973fedd4a2cba782f177076d7031
ethan-garcia <ethan.garcia@iest.edu.mx>
2026-03-13 08:41:32 -0600

    reloj digital

---
+++ Practica 4/Practica 4/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Horas = New System.Windows.Forms.Label()
+        Me.Minutos = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Dia = New System.Windows.Forms.Label()
+        Me.Fecha = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Horas
+        '
+        Me.Horas.AutoSize = True
+        Me.Horas.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Horas.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Horas.Location = New System.Drawing.Point(339, 111)
+        Me.Horas.Name = "Horas"
+        Me.Horas.Size = New System.Drawing.Size(78, 55)
+        Me.Horas.TabIndex = 0
+        Me.Horas.Text = "00"
+        '
+        'Minutos
+        '
+        Me.Minutos.AutoSize = True
+        Me.Minutos.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Minutos.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Minutos.Location = New System.Drawing.Point(466, 111)
+        Me.Minutos.Name = "Minutos"
+        Me.Minutos.Size = New System.Drawing.Size(78, 55)
+        Me.Minutos.TabIndex = 1
+        Me.Minutos.Text = "00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(423, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 55)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = ":"
+        '
+        'Dia
+        '
+        Me.Dia.AutoSize = True
+        Me.Dia.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Dia.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Dia.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Dia.Location = New System.Drawing.Point(346, 192)
+        Me.Dia.Name = "Dia"
+        Me.Dia.Size = New System.Drawing.Size(169, 55)
+        Me.Dia.TabIndex = 3
+        Me.Dia.Text = "Lunes "
+        '
+        'Fecha
+        '
+        Me.Fecha.AutoSize = True
+        Me.Fecha.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fecha.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Fecha.Location = New System.Drawing.Point(329, 247)
+        Me.Fecha.Name = "Fecha"
+        Me.Fecha.Size = New System.Drawing.Size(186, 25)
+        Me.Fecha.TabIndex = 4
+        Me.Fecha.Text = "09 de Marzo 2026"
+        '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-76, -111)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(992, 611)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer2
+        '
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.ErrorImage = Global.Practica_4.My.Resources.Resources.services_parametres_et_icone_d_engrenage_noir
+        Me.PictureBox2.Image = Global.Practica_4.My.Resources.Resources.services_parametres_et_icone_d_engrenage_noir
+        Me.PictureBox2.Location = New System.Drawing.Point(350, 278)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(52, 58)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(334, 90)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(233, 182)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Configuracion"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 21)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Fecha)
+        Me.Controls.Add(Me.Dia)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Minutos)
+        Me.Controls.Add(Me.Horas)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Horas As Label
+    Friend WithEvents Minutos As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Dia As Label
+    Friend WithEvents Fecha As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Practica 4/Practica 4/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim Date_o = DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy")
+        Dim Day_o = DateTime.Now.ToString("dddd")
+        Dim time_o = DateTime.Now.ToString("HH:mm")
+
+        Dim time_mod = time_o.Split(":")
+
+        Horas.text = time_mod(0)
+        Minutos.text = time_mod(1)
+        Dia.Text = Day_o
+        Fecha.text = Date_o
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label3.Text = "." Then
+            Label3.Text = ""
+        ElseIf Label3.Text = "" Then
+            Label3.Text = "."
+        End If
+    End Sub
+    Private Sub Fecha_Click(sender As Object, e As EventArgs) Handles Fecha.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        If Panel1.Visible = False Then
+            Panel1.Visible = True
+        ElseIf panel1.Visible = True Then
+            Panel1.Visible = False
+        End If
+    End Sub
+End Class
